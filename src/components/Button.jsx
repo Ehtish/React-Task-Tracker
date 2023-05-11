d4e5844509ce93731a60e9767ed2f32949194316
Ehtisham Ali <40139972+Ehtish@users.noreply.github.com>
2023-05-11 12:21:59 +0500

    make server and crud on api

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,22 +1,25 @@
-import React from "react";
-import PropTypes from "prop-types";
-export default function Button({ color, text }) {
-  const onClick = () => {
-    console.log("click");
-  };
+import PropTypes from 'prop-types'
+
+const Button = ({ color, text, onClick }) => {
   return (
-    <>
-      <button onClick={onClick} style={{ background: color }} className="btn">
-        {text}
-      </button>
-    </>
-  );
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className='btn'
+    >
+      {text}
+    </button>
+  )
 }
 
 Button.defaultProps = {
-  color: "steelblue",
-};
+  color: 'steelblue',
+}
+
 Button.propTypes = {
-  color: PropTypes.string,
   text: PropTypes.string,
-};
+  color: PropTypes.string,
+  onClick: PropTypes.func,
+}
+
+export default Button
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,36 @@
-import React from "react";
-import Button from "./Button";
+import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
+import Button from './Button'
 
-export default function Header({ title }) {
+const Header = ({ title, onAdd, showAdd }) => {
+  const location = useLocation()
+
   return (
-    <>
-      <header>
-        {/* <h2 style={headingStyle}>{title}</h2> */}
-        <h2>{title}</h2>
-        <Button color="green" text="Add" />
-      </header>
-    </>
-  );
+    <header className='header'>
+      <h1>{title}</h1>
+      {location.pathname === '/' && (
+        <Button
+          color={showAdd ? 'red' : 'green'}
+          text={showAdd ? 'Close' : 'Add'}
+          onClick={onAdd}
+        />
+      )}
+    </header>
+  )
 }
-Header.defaultProps = {
-  title: "Task Tracker",
-};
-// Header.propTypes = {
-//   title: propTypes.string,
-// };
 
-// css in js
+Header.defaultProps = {
+  title: 'Task Tracker',
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+// CSS in JS
 // const headingStyle = {
-//   color: "red",
-//   backgroundColor: "black",
-// };
+//   color: 'red',
+//   backgroundColor: 'black',
+// }
+
+export default Header
